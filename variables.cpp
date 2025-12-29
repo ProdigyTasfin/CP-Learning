6eb87715fc9ad436fbb182ff2fc0465c412b5f6a
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-29 15:18:20 +0600

    variables

---
+++ variables.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main() {
+    // Variable declarations
+    int a = 10;                      // Integer variable
+    double b = 5.5;                  // Double (floating-point) variable
+    float f = 3.14f;                 // Float variable
+    char c = 'A';                    // Character variable
+    string str = "I'm learning C++"; // String variable
+    bool flag = true;                // Boolean variable
+
+    // Output the variables
+    cout << "Integer a: " << a << endl;
+    cout << "Double b: " << b << endl;
+    cout << "Character c: " << c << endl;
+    cout << "Float f: " << f << endl;
+    cout << "String str: " << str << endl;
+    cout << "Boolean flag: " << (flag ? "true" : "false") << endl;
+
+    return 0;
+}
